8ee998f3804ac929af5d678c9f0f747bc879f0fc
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-04-22 21:27:47 +0800

    Update configs

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,14 +1,10 @@
-import _eslint from '@rollup/plugin-eslint';
-import _typescript from '@rollup/plugin-typescript';
+import eslint from '@rollup/plugin-eslint';
+import typescript from '@rollup/plugin-typescript';
 import { RollupOptions } from 'rollup';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 import outputSize from 'rollup-plugin-output-size';
 import pkg from './package.json' with { type: 'json' };
-
-// NOTE: remove once import errors are fixed for their respective packages
-const eslint = _eslint as unknown as typeof _eslint.default;
-const typescript = _typescript as unknown as typeof _typescript.default;
 
 // const PROD = process.env.NODE_ENV !== 'development';
 const WATCH = process.env.ROLLUP_WATCH === 'true';
@@ -25,12 +21,12 @@ export default defineConfig([
       { file: pkg.main, format: 'cjs', exports: 'named' },
       { file: pkg.module, format: 'esm' }
     ],
-    plugins: [esbuild({ target: 'esnext' }), outputSize()]
+    plugins: [esbuild({ target: 'esnext' }), outputSize({ bytes: true })]
   },
   {
     input,
     output: { file: pkg.types, format: 'esm' },
-    plugins: [dts(), outputSize()]
+    plugins: [dts(), outputSize({ bytes: true })]
   },
   WATCH && {
     input,
